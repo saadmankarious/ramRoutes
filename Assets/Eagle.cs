0eac02cdf93654092eb88ad479067214e7252278
Saad f/ New Macbook <you@example.com>
2025-04-14 00:42:54 -0500

    eagle animation and adding stars

---
+++ Assets/Eagle.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eagle : MonoBehaviour
+{
+    private bool filled;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Waterfall") )
+        {
+            animator.SetTrigger("cupFilling");
+            filled = true;
+            Debug.Log("filling cup + " + filled);
+
+        }
+        if (other.CompareTag("TreeSpot"))
+        {
+
+            Debug.Log("watering the tree + " + filled);
+            animator.SetTrigger("cupUnFilling");
+            
+        }
+    }
+
+}
