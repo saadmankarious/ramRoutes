16f7a4261bc62ca86c5432c2fdec8093daa6d097
Saad Mankarious <smankarious26@atsl10250.cornellcollege.edu>
2025-04-27 04:19:00 -0500

    adjusting drop item to avoid player contact

--- Assets/Scripts/Mechanics/TrashItem.cs
+++ Assets/Scripts/Mechanics/TrashItem.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class TrashItem : MonoBehaviour
 {
 
      private Animator animator;
      public string name;
+     public GameObject panel;
+     public Text text;
 
     void Start()
     {
@@ -15,11 +19,21 @@ public class TrashItem : MonoBehaviour
 
 void OnTriggerEnter2D(Collider2D other)
 {
-    if (other.CompareTag("Player") && gameObject.CompareTag("Eagle")) 
+    if (other.CompareTag("Player") && gameObject.CompareTag("Eagle") || other.CompareTag("Spaceship") && gameObject.CompareTag("Eagle")) 
     {
         animator.SetTrigger("turnIntoCup"); 
+        panel.SetActive(true);
+        text.text = "Pick up using 'C' key to water trees.";
         //TODO: Play just cup animation whenever the cup is getting picked up
         //TODO: add if statement before setting turn into cup uncondintionally
+    }
+}
+void OnTriggerExit2D(Collider2D other)
+{
+    if (other.CompareTag("Player") && gameObject.CompareTag("Eagle") || other.CompareTag("Spaceship") && gameObject.CompareTag("Eagle")) 
+    {
+        panel.SetActive(false);
+      
     }
 }
 
