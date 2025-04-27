f0a967a9edd4f8a8c59c84646f816819d9cb52a9
Saad Mankarious <smankarious26@atsl10250.cornellcollege.edu>
2025-04-27 10:49:01 -0500

    violets grow in place of picked up trash

--- Assets/Scripts/Mechanics/TrashItem.cs
+++ Assets/Scripts/Mechanics/TrashItem.cs
@@ -1,42 +1,66 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class TrashItem : MonoBehaviour
 {
+    private Animator animator;
+    public string name;
+    public GameObject panel;
+    public Text text;
 
-     private Animator animator;
-     public string name;
-     public GameObject panel;
-     public Text text;
+    [Header("Replacement Prefab")]
+    public GameObject replacementPrefab;
+
+    private Vector2 finalRestPosition;
+    private Rigidbody2D rb;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine(TrackRestingPosition());
     }
 
-void OnTriggerEnter2D(Collider2D other)
-{
-    if (other.CompareTag("Player") && gameObject.CompareTag("Eagle") || other.CompareTag("Spaceship") && gameObject.CompareTag("Eagle")) 
+    IEnumerator TrackRestingPosition()
     {
-        animator.SetTrigger("turnIntoCup"); 
-        panel.SetActive(true);
-        text.text = "Pick up using 'C' key to water trees.";
-        //TODO: Play just cup animation whenever the cup is getting picked up
-        //TODO: add if statement before setting turn into cup uncondintionally
+        while (true)
+        {
+            if (rb != null && rb.velocity.sqrMagnitude < 0.01f)
+            {
+                finalRestPosition = transform.position;
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
     }
-}
-void OnTriggerExit2D(Collider2D other)
-{
-    if (other.CompareTag("Player") && gameObject.CompareTag("Eagle") || other.CompareTag("Spaceship") && gameObject.CompareTag("Eagle")) 
+
+    void OnTriggerEnter2D(Collider2D other)
     {
-        panel.SetActive(false);
-      
+        if ((other.CompareTag("Player") || other.CompareTag("Spaceship")) && gameObject.CompareTag("Eagle")) 
+        {
+            animator.SetTrigger("turnIntoCup"); 
+            panel.SetActive(true);
+            text.text = "Pick up using 'C' key to water trees.";
+        }
     }
-}
 
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if ((other.CompareTag("Player") || other.CompareTag("Spaceship")) && gameObject.CompareTag("Eagle")) 
+        {
+            panel.SetActive(false);
+        }
+    }
 
-    
+    void OnDestroy()
+    {
+        if (replacementPrefab != null)
+        {
+            Instantiate(replacementPrefab, finalRestPosition, Quaternion.identity);
+        }
+        else
+        {
+            Debug.LogWarning("No replacement prefab assigned to TrashItem.");
+        }
+    }
 }
